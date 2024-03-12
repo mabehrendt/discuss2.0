e8ba1b05f15dad66eacb20ffdb2e7729a68df9cf
stefanwm13 <51990409+stefanwm13@users.noreply.github.com>
2024-03-12 22:09:15 +0100

    fixed stance detection behaviour. Users get suggested different comments now

--- apps/comments_async4/static/comments_async4/data_utils.jsx
+++ apps/comments_async4/static/comments_async4/data_utils.jsx
@@ -5,14 +5,15 @@ function addCreatorData(urlReplaces, props){
     console.log(props)
     console.log("CREATOR: " + props.user.user)
     console.log("HASH: " + cyrb53(props.user.user))
-     const stanceData = {
+    const stanceData = {
         urlReplaces: urlReplaces,
         content_type: props.subjectType,
         object_id: props.subjectId,
         creator: props.user.user,
         creator_id : cyrb53(props.user.user)
-      }
-      api.userstances.add(stanceData)
+    }
+    api.userstances.add(stanceData)
 }
 
 export {addCreatorData}
+// && stanceMap[stance.stance] !== stanceMap[_userStance]
