350285a073084310bd70125a75540af2833c9c00
Maike Behrendt <maike.behrendt@uni-duesseldorf.de>
2024-04-30 11:57:52 +0200

    script edits and new user table for pre-test with links

--- scripts/generate_mails.py
+++ scripts/generate_mails.py
@@ -7,7 +7,7 @@ file = 'import_users.csv'
 df = pd.read_csv(file,sep=',',dtype=str)
 
 for row in df.index:
-    mail = df.iloc[row]['username'] + str(df.iloc[row]['gruppe']) + '@hhu.de'
+    mail = df.iloc[row]['username'] + df.iloc[row]['gruppe'] + '@hhu.de'
     print(mail) 
     df.loc[row,'email'] = mail
 
