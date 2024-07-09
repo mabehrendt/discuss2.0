d4d465d667e71f1f445092feba61292c1d3a12b3
Maike Behrendt <maike.behrendt@uni-duesseldorf.de>
2024-07-09 13:57:17 +0200

    changed Netiqeutte Popup text

---
+++ adhocracy-plus/static/faq_text/NetiquettePopup.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function NetiquettePopup() {
+    return (
+        <div>
+            <ul>
+                <li><b>Bleiben Sie beim Thema:</b> Beziehen Sie Ihre Beiträge möglichst auf das Diskussionsthema.</li>
+                <li><b>Seien Sie respektvoll:</b> Machen Sie sich bitte bewusst, dass Sie auf der Diskussionsplattform in den Austausch mit anderen Menschen treten. Respektieren Sie die Meinungen anderer und greifen Sie niemanden persönlich an, auch wenn Sie einer Meinung nicht zustimmen.</li>
+                <li><b>Bleiben Sie fair:</b> Vermeiden Sie Provokationen oder Beleidigungen.</li>
+                <li><b>Beachten Sie das Urheberrecht:</b> Kennzeichnen Sie Zitate als solche.</li>
+                <li><b>Nehmen Sie konstruktiv teil:</b> Posten Sie keine Beiträge doppelt und kopieren Sie keine Beiträge anderer. Wenn möglich, erläutern Sie Ihre Standpunkte.</li>
+                <li><b>Wahren Sie die Persönlichkeitsrechte:</b> Teilen Sie keine persönlichen oder personenbezogenen Daten Dritter.</li>
+                <li><b>Achten Sie auf Verständlichkeit:</b> Verfassen Sie Ihre Beiträge und Kommentare in deutscher Sprache und versuchen Sie, sich verständlich auszudrücken.</li>
+                <li><b>Verlinken Sie nur geprüfte Inhalte:</b> Wenn Sie Verlinkungen verwenden, stellen Sie sicher, dass diese den Verhaltensregeln entsprechen.</li>
+            </ul>
+            <p><b>Das dulden wir nicht:</b></p>
+            <ul>
+                <li>Gewaltverherrlichende, menschenverachtende, rassistische, fremdenfeindliche, stigmatisierende, sexistische, religionsverachtende oder beleidigende Inhalte, Aufrufe zu Straftaten, Verleumdungen, geschäfts- oder rufschädigende Äußerungen sind nicht erlaubt.</li>
+                <li>Spam und Werbung sind nicht erlaubt.</li>
+                <li>Das Kopieren von Beiträgen oder Kommentaren anderer Nutzer:innen wird nicht geduldet und zählt nicht als Verfassen eines eigenen Beitrags.</li>
+                <li>Veröffentlichen Sie den gleichen Kommentar nicht mehrmals.</li>
+            </ul>
+            <p>Als Projektverantwortliche nehmen wir <b>nicht aktiv</b> an den Diskussionen teil. Wir <b>prüfen</b> aber sehr wohl alle Diskussionsbeiträge auf Einhaltung der Verhaltensregeln (Netiquette). Bei Verstößen können entsprechende Beiträge <b>gelöscht</b> werden. Wenn Sie Beiträge anderer Nutzer:innen lesen, die gegen die genannten Regeln verstoßen, können Sie diese durch einen Klick auf den Button „Melden“ melden. In diesem Falle wird die zuständige Moderation informiert und der betreffende Beitrag überprüft. Bei schweren oder wiederholten Verstößen gegen die Diskussionsregeln oder der missbräuchlichen Nutzung anderer Funktionen der Diskussionsplattform können einzelne <b>Nutzer:innen gesperrt</b> werden. Verstöße gegen die Diskussionsregeln können außerdem dazu führen, dass die Vergütungsbedingungen der Studie nicht erfüllt werden und Sie abschließend <b>keine Vergütung</b> ausgezahlt bekommen.</p>
+
+        </div>
+    );
+}
+
+export default NetiquettePopup;
