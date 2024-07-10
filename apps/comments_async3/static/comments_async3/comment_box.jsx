4f71ca9d2b7477055dd77a0fc0440e6abfffd5ca
Maike Behrendt <maike.behrendt@uni-duesseldorf.de>
2024-07-10 09:18:09 +0200

    no exclamation mark

--- apps/comments_async3/static/comments_async3/comment_box.jsx
+++ apps/comments_async3/static/comments_async3/comment_box.jsx
@@ -1100,7 +1100,7 @@ export const CommentBox = (props) => {
                 paddingLeft: "20px",
                 marginTop: "10px"
               }}>
-                <div className="argumentText"> Sobald genügend Teilnehmer:innen an der Diskussion beteiligt sind, bekommen Sie hier einen Kommentar vorgeschlagen, auf den Sie eingehen können!
+                <div className="argumentText"> Sobald genügend Teilnehmer:innen an der Diskussion beteiligt sind, bekommen Sie hier einen Kommentar vorgeschlagen, auf den Sie eingehen können.
                 </div>
               </div>
             </div>
