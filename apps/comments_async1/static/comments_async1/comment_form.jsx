640e1ed6741b024c475c8956b8a104b807056191
Maike Behrendt <maike.behrendt@uni-duesseldorf.de>
2024-07-17 21:49:28 +0200

    forgot the message

--- apps/comments_async1/static/comments_async1/comment_form.jsx
+++ apps/comments_async1/static/comments_async1/comment_form.jsx
@@ -16,6 +16,7 @@ const translated = {
   pleaseComment: django.gettext('Please login to comment'),
   onlyInvited: django.gettext('Only invited users can actively participate.'),
   notAllowedComment: django.gettext('The currently active phase doesn\'t allow to comment.')
+  participationEnded: django.gettext('The participation ended on the 03.08.2024. The voting phase will begin on the 05.08.2024.')
 }
 export default class CommentForm extends React.Component {
   constructor (props) {
